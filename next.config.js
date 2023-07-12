0d80211e138b88f7c256e8e32259d1335b6ac437
Kash5268 <kalmahanbetalihan@gmail.com>
2023-07-12 22:22:22 +0600

    initial

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["xzmaemcbymjbapvijlky.supabase.co"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
